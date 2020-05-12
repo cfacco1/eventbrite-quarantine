9a57dcdd2bf7603ee840c0f2b2cc87f654a61656
cfacco1 <facco.clement@gmail.com>
2020-05-12 18:10:52 +0200

    Update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,14 @@
 class EventsController < ApplicationController
+   before_action :authenticate_user!, only: [:new, :create, :show]
+   
   def index
     @events = Event.all
   end
 
   def show
+    @event = Event.find(params[:id])
   end
+
 
   def create
   end
