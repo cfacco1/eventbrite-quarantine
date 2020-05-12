5546e1ad0e66035e3a22b5ec96034920e0415c07
cfacco1 <facco.clement@gmail.com>
2020-05-12 14:19:53 +0200

    Update Devise

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,20 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def new
+  end
+end
