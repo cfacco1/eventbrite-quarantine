c091d40fd73dbbbfb4bd1a66295958751f278dc0
cfacco1 <facco.clement@gmail.com>
2020-05-12 22:06:49 +0200

    Finale

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
    before_action :authenticate_user!, only: [:new, :create, :show]
-   
+
   def index
     @events = Event.all
   end
@@ -11,6 +11,22 @@ class EventsController < ApplicationController
 
 
   def create
+    @event = Event.new('title' => params[:title], 
+      'description' => params[:description],
+      'duration' => params[:duration],
+      'start_date' => params[:start_date],
+      'price' => params[:price],
+      'location' => params[:location])
+    
+    if @event.save
+      puts "The event was succesfully saved !"
+      flash[:success] = "Britney OKAY"
+      redirect_to '/'
+    else
+      puts "Error : create problem"
+      flash[:danger] = "Britney Problem"
+      render '/events/new'
+    end
   end
 
   def update
@@ -20,5 +36,7 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
   end
+
 end
