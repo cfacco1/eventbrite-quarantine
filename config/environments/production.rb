ae3d44254c590507c409c81e03f8590b17919be5
cfacco1 <facco.clement@gmail.com>
2020-05-12 15:05:58 +0200

    up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'eventbrite-052020.herokuapp.com' }
+
 end
